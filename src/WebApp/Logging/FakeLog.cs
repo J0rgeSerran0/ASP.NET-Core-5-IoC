e31d18c4d7502f8d08235afbd2861192f864cda6
Jorge Serrano <jsp@spain.conseur.org>
2021-03-02 08:21:43 +0100

    IoC implementations in ASP.NET Core 5

--- src/WebApp/Logging/FakeLog.cs
+++ src/WebApp/Logging/FakeLog.cs
@@ -1,12 +1,19 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace WebApp.Logging
 {
-    public class FakeLog : ILog
+    public class FakeLog : ILog, IDisposable
     {
         private string _message = String.Empty;
 
         public void Append(string message) => _message += message;
+
+        public void Dispose()
+        {
+            Debug.Print($"{nameof(FakeLog)}.{nameof(Dispose)}");
+        }
+
         public string Get() => _message;
     }
 }
